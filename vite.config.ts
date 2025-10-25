aa514dcd94cc878adb16b6744b461ab8eb95d0df
Sebastian Giulietti <32080740+giuliettiseba@users.noreply.github.com>
2025-10-25 02:01:57 -0600

    refactor: remove unnecessary rollupOptions and chunkSizeWarningLimit from Vite build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'react-vendor';
-            }
-            if (id.includes('mui')) {
-              return 'mui-vendor';
-            }
-            return 'vendor';
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 800, // Increase limit if needed
-  },
 })
